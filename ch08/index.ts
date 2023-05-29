a000ebe477ce12649cf8252125080c609550ebbc
daolanfler <daolanfler@example.com>
2023-05-29 09:35:57 +0800

    chore: update

---
+++ ch08/index.ts
@@ -0,0 +1,2 @@
+// import "./buffer"; // 数组中存放的就是数据
+import "./window"; // higher order 数组中存放的的 Observables
---
+++ ch08/window.ts
@@ -0,0 +1,26 @@
+import { bufferTime, concat, concatAll, merge, mergeAll, take, timer, windowTime } from "rxjs";
+
+// the third Observable just starts subscribing, but never logs one value, and never completes.
+// because the timer is already completed, no value comming from upper stream.
+// timer(0, 1000)
+//   .pipe(windowTime(4000), take(3))
+//   .subscribe({
+//     complete: () => {
+//       console.log("the outer complete");
+//     },
+//     next: (ob) => {
+//       console.log("subscribing", ob);
+//       ob.subscribe({
+//         complete: () => {
+//           console.log("the inner complete");
+//         },
+//         next: console.log,
+//       });
+//     },
+//   });
+
+
+
+const source$ = timer(0, 1000);
+const result$ = source$.pipe(windowTime(4000, 2000, 3));
+result$.pipe(mergeAll()).subscribe(console.log)
