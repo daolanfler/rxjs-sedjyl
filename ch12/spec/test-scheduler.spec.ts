3ac8a3035b3a8393bc3942fbb74f5e59f60669ee
daolanfler <daolanfler@gmail.com>
2023-06-02 10:02:01 +0800

    chore: update

--- ch12/spec/test-scheduler.spec.ts
+++ ch12/spec/test-scheduler.spec.ts
@@ -1,33 +1,60 @@
 import { TestScheduler } from "rxjs/testing";
 import { assert, expect } from "chai";
-import { EMPTY, map, throwError } from "rxjs";
+import {
+  EMPTY,
+  NEVER,
+  concat,
+  concatWith,
+  interval,
+  map,
+  merge,
+  take,
+  throwError,
+} from "rxjs";
+import { observableMatcher } from "./helpers/observableMatcher";
 
 let scheduler: TestScheduler;
 
+// function shortcuts
+const addDrama = (x: number | string) => x + "!";
+
 describe("Observable", () => {
   beforeEach(() => {
-    scheduler = new TestScheduler(assert.deepEqual.bind(assert));
+    scheduler = new TestScheduler(observableMatcher);
   });
 
   it("should parse marble diagrams", () => {
-    const source = "--a--b--|";
-    const expected = "--a--b--|";
+    scheduler.run(({ expectObservable, cold }) => {
+      const source = "  --a--b--|";
+      const expected = "--a--b--|";
 
-    const source$ = scheduler.createColdObservable(source);
+      const source$ = cold(source);
 
-    scheduler.expectObservable(source$).toBe(expected);
-    scheduler.flush();
+      expectObservable(source$).toBe(expected);
+    });
   });
 
   it("should work with map operator", () => {
-    const source = "-a-b|";
-    const expected = "-a-b|";
+    scheduler.run(({ expectObservable, cold }) => {
+      const expected = "-a-b|";
+      const source$ = cold("-a-b|", { a: 1, b: 3 });
+      expectObservable(source$.pipe(map((x) => x * 2))).toBe(expected, {
+        a: 2,
+        b: 6,
+      });
+    });
+  });
 
-    const srouce$ = scheduler.createColdObservable(source, { a: 1, b: 3 });
-    scheduler
-      .expectObservable(srouce$.pipe(map((x) => x * 2)))
-      .toBe(expected, { a: 2, b: 6 });
-    scheduler.flush();
+  it("should map multiple values", () => {
+    scheduler.run(({ expectObservable, cold, expectSubscriptions }) => {
+      const e1 = cold(" --1--2--3--|");
+      const e1subs = "  ^----------!";
+      const expected = "--x--y--z--|";
+      const result = e1.pipe(map((x) => 10 * +x));
+
+      expectObservable(result).toBe(expected, { x: 10, y: 20, z: 30 });
+      expectSubscriptions(e1.subscriptions).toBe(e1subs);
+    });
   });
 
   it("create virtual time", () => {
@@ -43,10 +70,85 @@ describe("Observable", () => {
   });
 
   it("error stream", () => {
-    const srouce$ = throwError(() => "error")
+    const srouce$ = throwError(() => "error");
     const expected = "#";
 
     scheduler.expectObservable(srouce$).toBe(expected, undefined, "error");
     scheduler.flush();
+  });
+
+  it("never stream", () => {
+    const srouce$ = NEVER;
+    const expected = "-------";
+
+    scheduler.expectObservable(srouce$).toBe(expected);
+    scheduler.flush();
+  });
+
+  it("interval in TestScheduler", () => {
+    scheduler.run(({ expectObservable, cold }) => {
+      const source$ = interval(1, scheduler).pipe(
+        take(4),
+        map((x) => x.toString())
+      );
+      const expected = "-012(3|)";
+      expectObservable(source$).toBe(expected);
+    });
+  });
+
+  it("should work with merge operator", () => {
+    scheduler.run(({ expectObservable, cold }) => {
+      const source1 = " -a----b---|";
+      const source2 = " -c----d---|";
+      const expected = "-(ac)-(bd)|";
+      const source$1 = cold(source1);
+      const source$2 = cold(source2);
+      const merged$ = merge(source$1, source$2);
+      expectObservable(merged$).toBe(expected);
+    });
+  });
+
+  it("should work with hot Observable", () => {
+    scheduler.run(({ expectObservable, hot }) => {
+      const source1 = "-a-^b----|";
+      const source2 = "---^---c-|";
+      const expected = "  -b--c-|";
+
+      const source$1 = hot(source1);
+      const source$2 = hot(source2);
+
+      const merged$ = merge(source$1, source$2);
+      expectObservable(merged$).toBe(expected);
+    });
+  });
+
+  it("should work with both hot and cold Observable", () => {
+    scheduler.run(({ expectObservable, hot }) => {
+      const source1 = "-a-^b----|";
+      const source2 = "   --c-  |";
+      const expected = "  -bc---|";
+
+      const source$1 = hot(source1);
+      const source$2 = hot(source2);
+
+      const merged$ = merge(source$1, source$2);
+      expectObservable(merged$).toBe(expected);
+    });
+  });
+
+  it("check subscription marble", () => {
+    scheduler.run(({ expectObservable, cold, expectSubscriptions }) => {
+      const source1 = "     -a-b--|";
+      const source2 = "           -c--d-|";
+      const expectedRes = " -a-b---c--d-|";
+      const expectedSub1 = "^-----!";
+      const expectedSub2 = "------^-----!";
+
+      const src1$ = cold(source1);
+      const src2$ = cold(source2);
+      expectObservable(concat(src1$, src2$)).toBe(expectedRes);
+      expectSubscriptions(src1$.subscriptions).toBe(expectedSub1);
+      expectSubscriptions(src2$.subscriptions).toBe(expectedSub2);
+    });
   });
 });
